3e037cc255eae30140f5e115aa56415a8dd547fc
Jack Ullery <jmu17@pitt.edu>
2021-01-11 01:03:28 -0500

    Initial Commit.

---
+++ src/debuggable/DebugStream.java
@@ -0,0 +1,92 @@
+package misc;
+
+import java.io.IOException;
+import java.io.OutputStream;
+import java.io.PrintStream;
+
+/**
+ *
+ * @author Jack Ullery
+ */
+public class DebugStream extends OutputStream {
+
+    /**
+     * These are wrapped versions of common PrintStream variables. They can be
+     * easily imported, and used.
+     */
+    public final static PrintStream ERROR = wrapPrintStream(System.err);
+    public final static PrintStream ON = wrapPrintStream(System.out);
+    public final static PrintStream OFF = wrapPrintStream(null);
+
+    /* 
+     The number on the stack where the calling class should be located.
+     This is used to find the name of the class that called a particular method.
+     This constant is used for the debug_message() method. 
+    
+     This constant is a rather hacky solution. 
+     It has only been tested with certain values of println() and printf()
+     */
+    private static final int STACK_LEVEL = 10;
+
+    private final PrintStream output;
+    private final Boolean shouldPrint;
+
+    private DebugStream(PrintStream output, boolean val) {
+        this.output = output;
+        shouldPrint = val;
+    }
+
+    /**
+     * Returns a custom PrintStream object that wraps the input parameter.The
+     * returned object can be used like a normal PrintStream variable. When
+     * written to, it prepends a message noting the class the wrote the message.
+     * Furthermore, a null PrintStream parameter creates a dummy PrintStream,
+     * that writes nothing.
+     *
+     * @param stream The PrintStream to wrap with debugging data
+     * @return A new PrintStream that outputs debugging data whenever written to
+     */
+    public static PrintStream wrapPrintStream(PrintStream stream) {
+        if (stream != null) {
+            return new PrintStream(new DebugStream(stream, true), true);
+        }
+
+        return new PrintStream(new DebugStream(null, false));
+    }
+
+    @Override
+    public void write(byte[] b, int off, int len) {
+        if (shouldPrint) {
+            output.print(debug_message());
+            output.write(b, off, len);
+        }
+    }
+
+    @Override
+    public void write(byte[] b) throws IOException {
+        if (shouldPrint) {
+            output.print(debug_message());
+            output.write(b);
+        }
+    }
+
+    @Override
+    public void write(int b) {
+        if (shouldPrint) {
+            output.print(debug_message());
+            output.write(b);
+        }
+    }
+
+    private String debug_message() {
+        StackTraceElement[] trace = Thread.currentThread().getStackTrace();
+        String caller;
+
+        if (trace.length > STACK_LEVEL) {
+            caller = trace[STACK_LEVEL].getClassName();
+        } else {
+            caller = "Unknown Caller";
+        }
+        return String.format("[DEBUG] %-40.40s:", caller);
+    }
+}
---
+++ test/SimpleTest.java
@@ -0,0 +1,117 @@
+
+import java.io.ByteArrayOutputStream;
+import java.io.IOException;
+import java.io.PrintStream;
+import misc.DebugStream;
+import org.junit.After;
+import static org.junit.Assert.*;
+import org.junit.Before;
+import org.junit.Test;
+
+/**
+ *
+ * @author Jack Ullery
+ */
+public class SimpleTest {
+
+    private PrintStream dummy_output;
+    private ByteArrayOutputStream input;
+
+    private String data_string;
+    private int data_int;
+
+    @Before
+    public void setUp() {
+        input = new ByteArrayOutputStream();
+        dummy_output = DebugStream.wrapPrintStream(new PrintStream(input));
+        data_string = "This is some data to test with!";
+        data_int = -37;
+    }
+
+    @After
+    public void tearDown() throws IOException {
+        dummy_output.close();
+        input.close();
+    }
+
+    @Test
+    public void empty() {
+        assertEquals(0, input.size());
+    }
+
+    @Test
+    public void non_empty() {
+        dummy_output.println();
+        assertNotEquals(0, input.size());
+    }
+
+    public void checkPreamble(String ret) {
+        assertTrue(ret.contains("[DEBUG]"));
+        assertTrue(ret.contains("SimpleTest"));
+    }
+
+    @Test
+    public void check_println_empty() {
+        dummy_output.println();
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains("\n"));
+    }
+
+    @Test
+    public void check_println() {
+        dummy_output.println(data_string);
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains(data_string));
+        assertTrue(ret.contains("\n"));
+    }
+
+    @Test
+    public void check_print() {
+        dummy_output.print(data_string);
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains(data_string));
+        assertFalse(ret.contains("\n"));
+    }
+
+    @Test
+    public void check_printf() {
+        dummy_output.print(data_string);
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains(data_string));
+        assertFalse(ret.contains("\n"));
+    }
+
+    @Test
+    public void check_println_int() {
+        dummy_output.println(data_int);
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains(Integer.toString(data_int)));
+        assertTrue(ret.contains("\n"));
+    }
+
+    @Test
+    public void check_print_int() {
+        dummy_output.print(data_int);
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains(Integer.toString(data_int)));
+        assertFalse(ret.contains("\n"));
+    }
+
+    @Test
+    public void check_printf_int() {
+        dummy_output.print(data_int);
+        String ret = input.toString();
+        checkPreamble(ret);
+        assertTrue(ret.contains(Integer.toString(data_int)));
+        assertFalse(ret.contains("\n"));
+    }
+    
+    
+
+}
